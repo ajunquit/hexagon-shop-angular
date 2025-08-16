31bd489e73a2d9828dc6e5cffd8657c04cc090ee
Alejandro Junqui <ajunquit@gmail.com>
2025-08-15 23:58:53 -0500

    add order page

---
+++ apps/web-sites/polyshop-ng/src/app/orders/pages/orders-container/orders-container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-container',
+  imports: [],
+  templateUrl: './template/orders-container.html',
+  styleUrl: './orders-container.scss',
+})
+export class OrdersContainer {}
