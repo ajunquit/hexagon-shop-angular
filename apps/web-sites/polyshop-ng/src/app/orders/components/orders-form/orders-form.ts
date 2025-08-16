7dbd5b7ce2a304622d704569d7848bfb1d609a32
Alejandro Junqui <ajunquit@gmail.com>
2025-08-16 00:06:54 -0500

    create orders form and list

---
+++ apps/web-sites/polyshop-ng/src/app/orders/components/orders-form/orders-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-form',
+  imports: [],
+  templateUrl: './orders-form.html',
+  styleUrl: './orders-form.scss'
+})
+export class OrdersForm {
+
+}
