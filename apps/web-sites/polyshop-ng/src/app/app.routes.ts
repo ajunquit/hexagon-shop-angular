6427ee6e7682e5ef4f2c727c04934e1746e1faf0
Alejandro Junqui <ajunquit@gmail.com>
2025-08-15 21:25:06 -0500

    update routes

--- apps/web-sites/polyshop-ng/src/app/app.routes.ts
+++ apps/web-sites/polyshop-ng/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './auth/auth.routes';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+import { MainLayout } from './layout/components/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainLayout,
+    children: [...dashboardRoutes],
+  },
+  ...authRoutes,
+  { path: '**', redirectTo: 'dashboard' },
+];
