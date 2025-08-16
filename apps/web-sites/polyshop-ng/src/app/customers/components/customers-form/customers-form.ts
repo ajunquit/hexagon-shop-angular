97a23af6dcec1c8c12db04982da7f25ba032aba3
Alejandro Junqui <ajunquit@gmail.com>
2025-08-16 00:06:25 -0500

    create customer form and list

---
+++ apps/web-sites/polyshop-ng/src/app/customers/components/customers-form/customers-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-form',
+  imports: [],
+  templateUrl: './customers-form.html',
+  styleUrl: './customers-form.scss'
+})
+export class CustomersForm {
+
+}
