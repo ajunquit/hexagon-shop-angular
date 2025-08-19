e947ce3636a4154d0e35860ac426d93d40b90594
Alejandro Junqui <ajunquit@gmail.com>
2025-08-18 19:59:33 -0500

    add nav-group

---
+++ src/app/layout/components/nav-group/nav-group.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-group',
+  imports: [],
+  templateUrl: './nav-group.html',
+  styleUrl: './nav-group.scss'
+})
+export class NavGroup {
+
+}
